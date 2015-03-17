bc87b50e9956b25689e43ba191e25874b0b0aa46
Asa Toburen <atopettit@gmail.com>
2015-03-17 06:02:43 -0700

    on to testing

--- app/app.js
+++ app/app.js
@@ -2,47 +2,24 @@ angular.module('calendarDemoApp', [])
   .controller('mainAppCtrl', function($scope) {
 
     $scope.date = CalendarRange.getBasicDate();
-    console.log($scope);
   })
 
 .directive("calendar", function() {
   return {
     restrict: 'E',
-    template: '<div class="calendar-container"><div ng-class="{bookend : day.month !== date.month }" ng-repeat="day in days"><span>{{day.day}}</span></div></div>',
+    templateUrl: './calendar.html',
     replace: true,
     scope: true,
-    controller: function($scope, $element, $attrs) {
+    controller: function($scope) {
 
-      var dateParams = new Date(year, month);
+      $scope.$watch('date', function(date) {
+        
+        var dateParams = new Date(date.year, date.month);
 
-      var range = CalendarRange.getMonthlyRange(dateParams);
-
-      $scope.days = range.days;
-
-      $scope.$watch('date', function(newValue, oldValue) {
-
-      var months = $scope.date.months;
-
-    //I imagine that logic like this is best left to a controller,
-    // however, my intuition tells me that there is an easier way to get the
-    // month index..
-
-        if (typeof(newValue.month) != Number) {
-          monthIndex = months.indexOf(newValue.month);
-          dateParams = new Date(newValue.year, monthIndex);
-        } else {
-          dateParams = new Date(newValue.year, newValue.month);
-        }
-
-
-        console.log(dateParams);
-        //range = {};
-        //scope.days = null;
         range = CalendarRange.getMonthlyRange(dateParams);
-        //scope.days = range.days;
-        console.log(range);
 
-        //scope.apply(range);
+         $scope.days = range.days;
+         $scope.months = range.months;
 
       }, true);
     }
